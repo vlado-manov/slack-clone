e3dece82a7d68f5b694b9e6916fbb51ae561a56a
vlado-manov <vladimir.manov.bg@gmail.com>
2025-01-02 21:53:05 +0200

    Adding name to sign up process

--- src/components/convex-client-provider.tsx
+++ src/components/convex-client-provider.tsx
@@ -1,8 +1,8 @@
 "use client";
-import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 
-import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexAuthNextjsProvider } from "@convex-dev/auth/nextjs";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
